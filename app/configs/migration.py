ccb693206e0f1a7bef9f0584d5a5b014fbd80ec2
Keila Aparecida Rodrigues Passos <keila.rodrigues.passos@gmail.com>
2021-12-06 19:13:27 -0300

    feat(appointments_model): create appointment model

--- app/configs/migration.py
+++ app/configs/migration.py
@@ -6,5 +6,6 @@ def init_app(app: Flask):
     from app.models.professionals_model import ProfessionalsModel
     from app.models.patients_model import PatientModel
     from app.models.clinic_model import ClinicModel
+    # from app.models.appointment__model import AppointmentModel
 
     Migrate(app, app.db)
