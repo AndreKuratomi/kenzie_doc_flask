e363f961f441c9b71dbe23d3bea1c032b0a32fa0
Pierre Kalil <pierrekalil1@gmail.com>
2021-12-07 15:59:09 -0300

    feat(controler_pacient): create functions

--- app/controllers/pacients_controller.py
+++ app/controllers/pacients_controller.py
@@ -1,16 +1,56 @@
+from flask import jsonify, request, current_app
+from app.models.patients_model import PatientModel
+
 #criar paciente
 def create_pacient():
-    ...
+    data = request.json
+
+    pacient = PatientModel(**data)
+
+    current_app.db.session.add(pacient)
+    current_app.db.session.commit()
+
+    return jsonify(pacient), 201
 
 
 #listar pacientes
 def get_all_pacients():
-    ...
+    pacients = PatientModel.query.all()
+
+    serializer = [
+        {
+        "age":pacient.age,
+        "cpf":pacient.cpf,
+        "email":pacient.email,
+        "gender":pacient.gender,
+        "health_insurance":pacient.health_insurance,
+        "name": pacient.name,
+        "password":pacient.password,
+        "phone":pacient.phone
+        }for pacient in pacients
+    ]
+
+    return jsonify(serializer), 200
 
 
 #buscar um único paciente
-def get_one_pacient(cpf):
-    ...
+def filter_by_pacient(cpf:str):
+    pacients = PatientModel.query.filter_by(cpf=cpf)
+    print(pacients)
+    serializer = [
+        {
+        "age":pacient.age,
+        "cpf":pacient.cpf,
+        "email":pacient.email,
+        "gender":pacient.gender,
+        "health_insurance":pacient.health_insurance,
+        "name": pacient.name,
+        "password":pacient.password,
+        "phone":pacient.phone
+        }for pacient in pacients
+    ]
+
+    return jsonify(serializer), 200
 
 
 #buscar por data
@@ -19,10 +59,23 @@ def get_by_date():
 
 
 #atualizar paciente
-def update_pacient(cpf):
-    ...
+def update_pacient(cpf:str):
+    data = request.json
+
+    pacient = PatientModel.query.filter_by(cpf=cpf).update(data)
+
+    current_app.db.session.commit()
+
+    update_pacient = PatientModel.query.get(cpf)
+
+    return jsonify(update_pacient), 200
 
 
 #deletar paciente
-def delete_pacient(cpf):
-    ...
+def delete_pacient(cpf:str):
+    pacient = PatientModel.query.get(cpf)
+
+    current_app.db.session.delete(pacient)
+    current_app.db.session.commit()
+
+    return jsonify(pacient), 200
--- app/routes/pacients_blueprint.py
+++ app/routes/pacients_blueprint.py
@@ -1,11 +1,13 @@
 from flask import Blueprint
-from app.controllers.pacients_controller import create_pacient, update_pacient, delete_pacient, get_by_date
+from app.controllers.pacients_controller import create_pacient, update_pacient, delete_pacient, get_by_date, filter_by_pacient, get_all_pacients
 
 
 bp_pacients = Blueprint('bp_pacients', __name__, url_prefix='/pacients')
 
 bp_pacients.post('')(create_pacient)
-bp_pacients.patch('/<string:email>')(update_pacient)
-bp_pacients.delete('/<string:email>')(delete_pacient)
+bp_pacients.patch('/<string:cpf>')(update_pacient)
+bp_pacients.delete('/<string:cpf>')(delete_pacient)
 # nao sei bem como fazer esse de pegar pela data
+bp_pacients.get('')(get_all_pacients)
+bp_pacients.get('/<string:cpf>')(filter_by_pacient)
 bp_pacients.get('/<int:id>')(get_by_date)
