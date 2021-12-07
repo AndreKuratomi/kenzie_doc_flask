8af942ed874c04cc1ade3aa5f1682df7654b5bbf
André Kuratomi <andrekuratomi@gmail.com>
2021-12-07 13:40:41 -0300

    feat(appointments_model.py): Relation N:N added to script

--- app/models/appointments_model.py
+++ app/models/appointments_model.py
@@ -1,10 +1,11 @@
 from app.configs.database import db
 from dataclasses import dataclass
 
+
 @dataclass
 class AppointmentsModel(db.Model):
 
-    id = int
+    id: int
     clinic_id: int
     pacient_id: str
     date: str
@@ -13,8 +14,10 @@ class AppointmentsModel(db.Model):
     __tablename__ = 'appointments'
 
     id = db.Column(db.Integer, primary_key=True)
-    clinic_id = db.Column(db.Integer,db.ForeignKey("clinics.id"))
-    pacient_id = db.Column(db.String(20), db.ForeignKey("patients.cpf"))
     date = db.Column(db.DateTime, nullable=False)
     finished = db.Column(db.Boolean, default=False)
 
+    patient_id = db.Column(db.String(11), db.ForeignKey("patients.cpf"), nullable=False, unique=True)
+    professionals_id = db.Column(db.String(20), db.ForeignKey("professionals.council_numbers"), nullable=False, unique=True)
+
+    # clinic_id = db.Column(db.Integer,db.ForeignKey("clinics.id"))
