68c9401c03d4b5f66a04ebfc21aea92490cf724a
André Kuratomi <andrekuratomi@gmail.com>
2021-12-09 11:01:36 -0300

    fix: create_patient method revised as well as ipdb lib added

--- app/controllers/patients_controller.py
+++ app/controllers/patients_controller.py
@@ -3,35 +3,42 @@ from app.models.appointments_model import AppointmentsModel
 from app.models.patients_model import PatientModel
 from sqlalchemy.exc import IntegrityError
 import re
+from ipdb import set_trace
 
-# criar patiente
+
+# criar paciente
 def create_patient():
     try:
-        data = request.json
+        data = request.get_json()
+        required_keys = ['cpf', 'name', 'email', 'phone', 'password', 'age', 'gender', 'health_insurance']
 
-        required_keys = ['cpf', 'name', 'email',
-                        'phone', 'password', 'age', 'gender', 'health_insurance']
-        data = request.json
+        keys = data.keys()
+        set_keys = set(keys)
+        set_required_keys = set(required_keys)
+        diff = set_required_keys.difference(set_keys)
 
-        for key in data:
-            if key not in required_keys:
-                return {"msg": f"The key {key} is not valid"}, 400
+        if diff != set():
+            if len(diff) == 1:
+                return {"error": f"The key {diff} is not present."}, 400
+            else:
+                return {"error": f"The keys {diff} are not present."}, 400
+        # set_trace()
 
         if type(data['cpf']) != str or type(data['name']) != str or type(data['email']) != str or type(data['phone']) != str or type(data['password']) != str or type(data['gender']) != str or type(data['health_insurance']) != str:
-                return {"msg": "Numeric data is invalid. Text field only"}, 400
+            return {"error": "Invalid type data. Fields other than 'age' must be only strings"}, 400
         
         if type(data['age']) != int:
-            return {"msg": "Invalid field age. "}
+            return {"error": "Invalid field 'age'. It must be an integer"}
 
         if not re.fullmatch(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', data['email']):
-            return {"msg": "Invalid email. Correct example yourname@provider.com"}, 400
+            return {"error": "Invalid email. Correct example yourname@provider.com"}, 400
 
-        if not re.fullmatch(r'\(\d{2,}\)\d{4,}\-\d{4}', data['phone']):
-            return {"msg": "Invalid phone number"}, 400 
+        if not re.fullmatch(r'\d{3}\.\d{3}\.\d{3}\-\d{2}', data['cpf']):
+            return {"error": "Invalid CPF format. Correct example: xxx.xxx.xxx-xx"}, 400 
         
         validate_phone = re.fullmatch(r"^(\([0-9]{2}\)[0-9]{5}-)[0-9]{4}$", data['phone'])
         if validate_phone == None:
-            return jsonify({"error": "Invalid phone number format. Correct example (xx)xxxxx-xxxx"})
+            return {"error": "Invalid phone number format. Correct example: (xx)yxxxx-xxxx"}, 400
 
         patient = PatientModel(**data)
 
@@ -39,8 +46,9 @@ def create_patient():
         current_app.db.session.commit()
 
         return jsonify(patient), 201
+
     except IntegrityError:
-        return {"msg": "Patient aleary exisits"}
+        return {"msg": "Patient already exists. Please check CPF and email."}, 409
 
 
 # listar patientes
