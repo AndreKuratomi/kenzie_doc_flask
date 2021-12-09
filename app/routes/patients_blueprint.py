ee5e56c85d992ef8f92bfeecd7d8f82fc14e5c39
André Kuratomi <andrekuratomi@gmail.com>
2021-12-09 19:17:45 -0300

    refactor(routes/): blueprints put in CRUD order

--- app/routes/patients_blueprint.py
+++ app/routes/patients_blueprint.py
@@ -5,9 +5,9 @@ from app.controllers.patients_controller import create_patient, update_patient, 
 bp_patients = Blueprint('bp_patients', __name__, url_prefix='/patients')
 
 bp_patients.post('')(create_patient)
+bp_patients.get('')(get_all_patients)
+# nao sei bem como fazer esse de pegar pela data
+# bp_patients.get('/<int:id>')(get_by_date)
+bp_patients.get('/<string:cpf>')(filter_by_patient)
 bp_patients.patch('/<string:cpf>')(update_patient)
 bp_patients.delete('/<string:cpf>')(delete_patient)
-# nao sei bem como fazer esse de pegar pela data
-bp_patients.get('')(get_all_patients)
-bp_patients.get('/<string:cpf>')(filter_by_patient)
-# bp_patients.get('/<int:id>')(get_by_date)
