9263d8f267a5cdcd9b4108306c1a00fd1200f3dc
Keila Aparecida Rodrigues Passos <keila.rodrigues.passos@gmail.com>
2021-12-15 14:45:53 -0300

    fix(api_blueprint): commenting blueprint appointment

--- app/routes/api_blueprint.py
+++ app/routes/api_blueprint.py
@@ -1,11 +1,11 @@
 from flask import Blueprint
-from . import patients_blueprint, professionals_blueprint, appointments_blueprint, login_blueprint, secretary_blueprint
+from . import patients_blueprint, professionals_blueprint,login_blueprint, secretary_blueprint
 
 
 bp = Blueprint('api_bp', __name__, url_prefix='')
 
 bp.register_blueprint(professionals_blueprint.bp_professionals)
 bp.register_blueprint(patients_blueprint.bp_patients)
-bp.register_blueprint(appointments_blueprint.bp_appointments)
+# bp.register_blueprint(appointments_blueprint.bp_appointments)
 bp.register_blueprint(login_blueprint.bp_login)
 bp.register_blueprint(secretary_blueprint.bp_secretary)
